bc960968ec0763230be16e58c46ae1d0fc7b4bfe
Madhurima Mishra <mail2sikun@gmail.com>
2020-09-13 16:57:04 +0530

    app.js modified

--- counter/app.js
+++ counter/app.js
@@ -1,17 +1,17 @@
 let count=0;
 const value=document.querySelector("#value");
 const btns=document.querySelectorAll(".btn");
+
 btns.forEach(function(btn){
-    btn.addEventListener("cilck",function(e){
-      const styles=e.currentTarget.classList;
-      if(styles.contains("increase")){
-          count++;
-      }else if(styles.contains("decrease")){
-          count --;
-      }else{
-          count=0;
-      }
-     
+    btn.addEventListener("click",function (e){
+        const styles=e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count --;
+        }else if(styles.contains("increase")){
+            count ++;
+        }else {
+            count=0;
+        }  
     if (count > 0) {
         value.style.color = "green";
       }
