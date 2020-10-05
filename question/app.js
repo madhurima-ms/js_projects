6a381e4e8e3c5047d30ad36fe982bd536ca960f6
Madhurima Mishra <mail2sikun@gmail.com>
2020-10-05 23:06:35 +0530

    files added

--- question/app.js
+++ question/app.js
@@ -2,6 +2,7 @@ const questions = document.querySelectorAll(".question");
 
 questions.forEach(function(question){
   const btn= question.querySelector(".question-btn");
+  console.log(btn);
 
 btn.addEventListener("click", function() {
 console.log(questions);
