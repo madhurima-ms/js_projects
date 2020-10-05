d5c3d294e0e4bd33b04633dbde2a48b36618e07d
Madhurima Mishra <mail2sikun@gmail.com>
2020-10-05 22:55:43 +0530

    files added

--- question/app.js
+++ question/app.js
@@ -1,19 +1,43 @@
-const questions=document.querySelectorAll(".question");
+// const questions=document.querySelectorAll(".question");
 
-questions.forEach(function(question){
-  const btn= question.querySelector(".question-btn");
-btn.addEventListner("click", function() {
-  questions.forEach(function(item){
-    if(item!==question){
-      item.classList.remove("show-text");
-    }
+// questions.forEach(function(question){
+//   const btn= question.querySelector(".question-btn");
+
+// btn.addEventListner("click", function() {
+
+//   questions.forEach(function(item){
+//     if(item!==question){
+//       item.classList.remove("show-text");
+//     }
+//   });
+//   question.classList.toggle("show-text");
+// });
+
+// });
+
+
+
+
+
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
   });
-  question.classList.toggle("show-text");
 });
-
-});
-
-
 
 
 
