e7c1e087fffb6a8b0bbe8e73ba09ad7705f83381
Madhurima Mishra <mail2sikun@gmail.com>
2020-10-11 20:14:13 +0530

    files added

--- video/app.js
+++ video/app.js
@@ -1,19 +1,40 @@
-const btn = document.querySelector(".switch-btn");
-const video = document.querySelector(".video-container");
+// const btn = document.querySelector(".switch-btn");
+// const video = document.querySelector(".video-container");
 
-btn.addEventListener("click", function () {
-  if (!btn.classList.contains("slide")) {
+// btn.addEventListener("click", function () {
+//   if (!btn.classList.contains("slide")) {
+//     btn.classList.add("slide");
+//     video.play();
+//   } else {
+//     btn.classList.remove("slide");
+//     video.pause();
+//   }
+// });
+
+// // preloader
+// const preloader = document.querySelector(".preloader");
+
+// window.addEventListener("load", function () {
+//   preloader.classList.add("hide-preloader");
+// });
+
+
+const btn= document.querySelector(".switch-btn");
+const video= document.querySelector(".video-container");
+
+btn.addEventListener("click",function(){
+  if(!btn.classList.contains("slide")){
     btn.classList.add("slide");
-    video.play();
-  } else {
-    btn.classList.remove("slide");
     video.pause();
   }
+  else{
+    btn.classList.remove("slide");
+    Video.play();
+
+  }
 });
-
-// preloader
-const preloader = document.querySelector(".preloader");
-
-window.addEventListener("load", function () {
+ 
+const preloader=document.querySelector(".preloader")
+window.addEventListener("load", function() {
   preloader.classList.add("hide-preloader");
 });
