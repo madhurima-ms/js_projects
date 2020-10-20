f43f68257bdb54fd5488230a90cb400640028b0a
Madhurima Mishra <mail2sikun@gmail.com>
2020-10-20 12:10:49 +0530

    text replaced

--- loremPara/app.js
+++ loremPara/app.js
@@ -40,9 +40,9 @@ form.addEventListener("submit",function(e){
   e.preventDefault();
 
   const value= parseInt(amount.value);
-  const random = Math.floor(Math.random()*text.length);
-if (isNaN(value) || value < 0 || value > 9){
-result.innerHTML= '<p class="result"> ${item[random]}</p>';
+  const random = Math.floor(Math.random() * text.length);
+if (isNaN(value) || value < 0 || value > 9) {
+result.innerHTML= '<p class="result"> ${text[random]}</p>';
 }else{
   let tempText = text.slice(0,value);
   tempText = tempText
