af7e0c8fb7acda80c1300fbad6fa22ba3c427aae
Madhurima Mishra <mail2sikun@gmail.com>
2020-10-20 16:33:48 +0530

    consoling the log

--- loremPara/app.js
+++ loremPara/app.js
@@ -42,14 +42,16 @@ form.addEventListener("submit", function(e){
   const value= parseInt(amount.value);
  
  const random = Math.floor(Math.random()* text.length);
- console.log(random);
-if(isNaN(value) || value<0 || value>9){
+ 
+if(isNaN(value) || value<0 || value > 9){
    result.innerHTML= `<p class="result"> ${text[random]}</p>`;
-} else{
+} else {
 let tempText = text.slice(0, value);
 tempText=tempText.map(function(item){
   return `<p class="result>"${item}</p>`;
 })
+result.innerHTML=tempText;
+console.log(tempText);
 }
 
 
