94fc0b0d6da77f6c2d1cf40d375373429f1fe648
Madhurima Mishra <mail2sikun@gmail.com>
2020-10-20 13:24:37 +0530

    tilt added

--- loremPara/app.js
+++ loremPara/app.js
@@ -15,44 +15,37 @@ const form = document.querySelector(".lorem-form");
 const amount = document.getElementById("amount");
 const result = document.querySelector(".lorem-text");
 
-// form.addEventListener("submit", function (e) {
-//   // A click on a form submit button – initiates its submission to the server.
-
+// form.addEventListener("submit",function(e){
 //   e.preventDefault();
 
-//   const value = parseInt(amount.value);
+//   const value= parseInt(amount.value);
 //   const random = Math.floor(Math.random() * text.length);
+// if (isNaN(value) || value < 0 || value > 9) {
+//   result.innerHTML= `<p class="result"> ${text[random]}</p>`;
+// } else {
+//   let tempText = text.slice(0,value);
+//    tempText = tempText
+//   .map(function(item){
 
-//   if (isNaN(value) || value < 0 || value > 9) {
-//     result.innerHTML = `<p class="result">${text[random]}</p>`;
-//   } else {
-//     let tempText = text.slice(0, value);
-//     tempText = tempText
-//       .map(function (item) {
-//         return `<p class="result">${item}</p>`;
-//       })
-//       .join("");
-//     result.innerHTML = tempText;
-//   }
-//  });
+//     return `<p class="result">${item}</p>`;
+    
+//   })
+//   .join("");
+//   result.innerHTML = tempText;
+// }
 
-form.addEventListener("submit",function(e){
+// });
+
+form.addEventListener("submit", function(e){
   e.preventDefault();
 
   const value= parseInt(amount.value);
-  const random = Math.floor(Math.random() * text.length);
-if (isNaN(value) || value < 0 || value > 9) {
-  result.innerHTML= `<p class="result"> ${text[random]}</p>`;
-} else {
-  let tempText = text.slice(0,value);
-   tempText = tempText
-  .map(function(item){
+  const random = Math.floor(Math.random()* text.length);
+if(isNaN(value) || value<0 || value>9){
+   result.innerHTML= `<p class="result"> ${text[random]}</p>`;
+} else{
 
-    return `<p class="result">${item}</p>`;
-    
-  })
-  .join("");
-  result.innerHTML = tempText;
 }
 
-});
+
+})
